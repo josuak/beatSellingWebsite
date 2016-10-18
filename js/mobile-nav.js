b57bf3b452816f30155c2ddb50866a721bd9000f
Josua Krämer <kraemerjosua@gmail.com>
2016-10-18 15:08:28 +0200

    Some changes
    - When you click outside the mobile nav it closes

---
+++ js/mobile-nav.js
@@ -0,0 +1,32 @@
+var nav = document.getElementById("nav");
+var navToggleSwitch = document.getElementById("nav-toggle-switch");
+
+function navSwitchToggle() {
+    if (navToggleSwitch.className === "normal") {
+        navToggleSwitch.className = "active";
+        
+    } else {
+        navToggleSwitch.className = "normal";
+    }
+}
+
+function navToggle() {
+    if (nav.className === "hidden") {
+        nav.className = "visible";
+    } else {
+        nav.className = "hidden";
+    }
+}
+
+window.addEventListener("mouseup", function (event) {
+    if (event.target !== nav && event.target.parentNode !== nav && event.target.parentNode.parentNode !== nav && event.target.parentNode.parentNode.parentNode !== nav && event.target !== navToggleSwitch && event.target.parentNode !== navToggleSwitch) {
+        nav.className = "hidden";
+        navToggleSwitch.className = "normal";
+    }
+});
+
+
+
+
+
+            
