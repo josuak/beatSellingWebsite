877cb398a9001c6f95421eb7ab7f2a597826b951
Josua Krämer <kraemerjosua@gmail.com>
2016-10-21 17:32:58 +0200

    Added background image to banner

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -19,7 +19,7 @@ function navToggle() {
 }
 
 window.addEventListener("mouseup", function (event) {
-    if (event.target !== nav && event.target.parentNode !== nav && event.target.parentNode.parentNode !== nav && event.target.parentNode.parentNode.parentNode !== nav && event.target !== navToggleSwitch && event.target.parentNode !== navToggleSwitch) {
+    if (event.target !== nav && event.target.parentNode !== nav && event.target.parentNode.parentNode !== nav && event.target.parentNode.parentNode.parentNode !== nav && event.target.parentNode.parentNode.parentNode.parentNode !== nav && event.target !== navToggleSwitch && event.target.parentNode !== navToggleSwitch) {
         nav.className = "hidden";
         navToggleSwitch.className = "normal";
     }
